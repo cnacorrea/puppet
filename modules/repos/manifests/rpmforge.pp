923a4ca72c9755d50fa61425a3f87c90f63866e4
cnacorrea <carlos.nilton@gmail.com>
2013-09-09 17:18:18 -0300

    download rpmforge thru wget

--- modules/repos/manifests/rpmforge.pp
+++ modules/repos/manifests/rpmforge.pp
@@ -1,6 +1,7 @@
 class repos::rpmforge {
-	exec { 'rpm -ivh --force http://78.46.17.228/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm':
-		cwd  => "/tmp",
-		path => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
+	exec { 'wget -O rpmforge.rpm http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm':
+		cwd     => "/tmp",
+		path    => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
+		creates => "/tmp/rpmforge.rpm",
 	}
 }
