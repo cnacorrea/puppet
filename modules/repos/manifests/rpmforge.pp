5ed7dad40cabd3b8ab6fd2be64f2e9651a3d5b3b
cnacorrea <carlos.nilton@gmail.com>
2013-09-09 17:56:45 -0300

    install rpmforge locally

--- modules/repos/manifests/rpmforge.pp
+++ modules/repos/manifests/rpmforge.pp
@@ -1,11 +1,20 @@
 class repos::rpmforge {
-	exec { 'rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt':
-		cwd     => "/tmp",
-		path    => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
+	file { '/tmp/RPM-GPG-KEY.dag.txt':
+		source  => "puppet:///modules/repos/RPM-GPG-KEY.dag.txt",
+		creates => '/tmp/RPM-GPG-KEY.dag.txt',
+		notify  => Exec['rpmforge-install'],
 	}
 
-	exec { 'yum install http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm':
+	file { '/tmp/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm':
+		source  => "puppet:///modules/repos/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm",
+		creates => '/tmp/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm',
+		notify  => Exec['rpmforge-install'],
+	}
+
+	exec { 'rpmforge-install':
+		command     => 'rpm --import /tmp/RPM-GPG-KEY.dag.txt && yum localinstall /tmp/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm',
 		cwd         => "/tmp",
 		path        => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
+		refreshonly => true,
 	}
 }
