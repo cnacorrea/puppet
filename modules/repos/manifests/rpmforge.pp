0422fd7a69387e07b510d8c0fe045cce364ee811
cnacorrea <carlos.nilton@gmail.com>
2013-09-09 17:35:07 -0300

    install local pack rpmforge

--- modules/repos/manifests/rpmforge.pp
+++ modules/repos/manifests/rpmforge.pp
@@ -1,5 +1,5 @@
 class repos::rpmforge {
-	exec { 'curl -o rpmforge.rpm http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm':
+	exec { 'curl -o rpmforge.rpm http://rpmforge.sw.be/redhat/el6/en/rpmforge/rpmforge/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm':
 		cwd     => "/tmp",
 		path    => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
 		creates => "/tmp/rpmforge.rpm",
