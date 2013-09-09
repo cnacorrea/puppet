498c052fd1bb5655a79a81b3127e73c7b73e8325
cnacorrea <carlos.nilton@gmail.com>
2013-09-09 18:08:17 -0300

    fix typo

--- modules/repos/manifests/rpmforge.pp
+++ modules/repos/manifests/rpmforge.pp
@@ -1,13 +1,11 @@
 class repos::rpmforge {
 	file { '/tmp/RPM-GPG-KEY.dag.txt':
 		source  => "puppet:///modules/repos/RPM-GPG-KEY.dag.txt",
-		creates => '/tmp/RPM-GPG-KEY.dag.txt',
 		notify  => Exec['rpmforge-install'],
 	}
 
 	file { '/tmp/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm':
 		source  => "puppet:///modules/repos/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm",
-		creates => '/tmp/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm',
 		notify  => Exec['rpmforge-install'],
 	}
 
