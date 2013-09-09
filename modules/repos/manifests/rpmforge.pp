5eb8b2c457b934ae419cc2e8013363337aeef9af
cnacorrea <carlos.nilton@gmail.com>
2013-09-09 17:13:48 -0300

    change rpmforge rpm url

--- modules/repos/manifests/rpmforge.pp
+++ modules/repos/manifests/rpmforge.pp
@@ -1,5 +1,5 @@
 class repos::rpmforge {
-	exec { 'rpm -ivh --force http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm':
+	exec { 'rpm -ivh --force http://78.46.17.228/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm':
 		cwd  => "/tmp",
 		path => [ "/bin", "/sbin", "/usr/bin", "/usr/sbin" ],
 	}
