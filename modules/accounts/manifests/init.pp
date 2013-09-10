81bfebd28777c7656fee01c3f69064f8f7e5bf8d
cnacorrea <carlos.nilton@gmail.com>
2013-09-10 10:34:50 -0300

    first accounts version

---
+++ modules/accounts/manifests/init.pp
@@ -0,0 +1,36 @@
+class accounts {
+	add_user { fed5:
+		name       => "Carlos 'Bill' Nilton",
+		shell      => "/bin/bash",
+		sshkeytype => "ssh-rsa",
+		sshkey     => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCwDpVxbyevf68EFJIVMnA83H0gLMMrgZpAoqVNcm3TqJKvYrdXrc3SYAKkjOrqAC5pNHmht1T/bAmLjXLodaQFx702ek3A8V3o5gvnnW90mvGc78/qq3VCG8ytLiR+NS6sg2w/njCuMM4qYsFy8Mj6S7jBYePvx1DpjrEywabc/4llpCeP6H/USZmGnMWDsVanRBE2YzkL/lFRKz1QCQpvOpCZdxQMg6YUkRH63cb0RrWJsTQQuHY566Df8xwdtBycv6Cn3PhtLJbq9xMXcDEpHxPtLLNuL93P5bWCS/DtzQcvCqRzVSiAaiQwg7OAbo4C4LSIBlZKZukiLvJ9keoH",
+	}
+}
+
+define add_user($name, $uid, $password, $shell, $groups, $sshkeytype, $sshkey) {
+	$username = $title
+
+	user { $username:
+		comment => "$name",
+		home    => "/home/$username",
+		shell   => "$shell",
+	}
+
+	if $sshkeytype != 'none' {
+		ssh_authorized_key{ $username:
+			user   => "$username",
+			ensure => present,
+			type   => "$sshkeytype",
+			key    => "$sshkey",
+			name   => "$username"
+		}
+
+		ssh_authorized_key{ "confman-${username}":
+			user   => "confman",
+			ensure => present,
+			type   => "$sshkeytype",
+			key    => "$sshkey",
+			name   => "confman-${username}"
+		}
+	}
+}
