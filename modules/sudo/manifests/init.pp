658b1b092ecbcffdfaf6af682b793da5b4909005
cnacorrea <carlos.nilton@gmail.com>
2013-09-11 21:07:07 -0300

    trying a new approach to sudo

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,15 +1,5 @@
-class sudo::install {
-	@package { sudo:
+class sudo {
+	package { sudo:
 		ensure => present,
-	}
-}
-
-define sudo::add_user($user) {
-	search Sudo::Install
-	realize(Package['sudo'])
-
-	append_if_no_such_line { "$user":
-		file => "/etc/sudoers",
-		line => "${user} ALL=(ALL) NOPASSWD: ALL",
 	}
 }
