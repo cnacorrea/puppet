1e827dfd521c218b6d2d6d9f7b364627bf683490
cnacorrea <carlos.nilton@gmail.com>
2013-09-12 15:54:42 -0300

    another structure test

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,30 @@
-import 'nodes.pp'
+node default {
+	include puppet
+	include sudo
+}
+
+node unimed inherits default {
+	include user::virtual
+	include user::redes-unimed
+}
+
+node unimed-banco inherits unimed {
+	include user::dba-unimed
+}
+
+node 'billmbp' {
+	include puppet
+}
+
+node 'otrs' inherits default {
+	include repos::rpmforge
+}
+
+node rjofedlx212 inherits unimed-banco {
+}
+
+node rjofedlx213 inherits unimed-banco {
+}
+
+node rjofedlx214 inherits unimed-banco {
+}
