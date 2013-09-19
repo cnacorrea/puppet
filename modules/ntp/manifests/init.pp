784b4b995df21b209907f013796145e861b66b87
cnacorrea <carlos.nilton@gmail.com>
2013-09-19 11:48:38 -0300

    disable ntp for virtual machines

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -2,6 +2,15 @@ class ntp {
 	if $::virtual == physical {
 		include ntp::params, ntp::install, ntp::config, ntp::service
 	} else {
-		warn("Tried to enable ntp on a ${::virtual} virtual machine. For the sake of reliability, I'm NOT doing it. Please, check ${::virtual} good practices for timekeeping.")
+		warn("Tried to enable ntp on a ${::virtual} virtual machine. For the sake of reliability, I'm DISABLING it. Please, check ${::virtual} good practices for timekeeping.")
+
+		include ntp::params
+
+		service { $ntp::params::ntp_service_name:
+			ensure => stopped,
+			hasstatus => true,
+			hasrestart => true,
+			enable => false,
+		}
 	}
 }
