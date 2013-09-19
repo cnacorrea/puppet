cbf90ddc34f1bd9eed74f2022927daca8b0cc91f
cnacorrea <carlos.nilton@gmail.com>
2013-09-19 12:24:06 -0300

    playing nicely with variables

--- modules/ntp/manifests/config.pp
+++ modules/ntp/manifests/config.pp
@@ -30,7 +30,7 @@ class ntp::config {
 	if $::osfamily == 'RedHat' {
 		augeas { 'ntp-options':
 			context => '/files/etc/sysconfig/ntpd',
-			changes => 'set OPTIONS \'"-u ntp:ntp -p /var/run/ntpd.pid -g -c ${ntp::params::ntp_config_file}"\'',
+			changes => 'set OPTIONS \'"-u ntp:ntp -p /var/run/ntpd.pid -g -c \'${ntp::params::ntp_config_file}\'"\'',
 			notify  => Class["ntp::service"],
 		}
 	}
