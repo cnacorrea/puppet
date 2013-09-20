a9af656358b3d336fab62dbdc150ce7c851a4649
cnacorrea <carlos.nilton@gmail.com>
2013-09-20 12:29:03 -0300

    add jboss password management script

--- modules/jboss711/files/create_jboss_password.sh
+++ modules/jboss711/files/create_jboss_password.sh
@@ -5,7 +5,7 @@ PASS=`/usr/local/sbin/generate_crypt_string.sh`
 MAIL="$1"
 
 cd /opt/jboss
-bin/add-user.sh --silent=true javamgmt $PASS
+bin/add-user.sh --silent=true javamgmt $PASS &> /tmp/teste.log
 
 mail -s "Your new JBoss admin credentials" $MAIL << EOF
 Dear JBoss user,
