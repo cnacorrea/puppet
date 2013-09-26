22785c436e0633b7c7dc83eccdb536ee033da672
cnacorrea <carlos.nilton@gmail.com>
2013-09-26 15:38:46 -0300

    first version of ruby applications class

--- modules/rubyapp/files/start-rubyapp.sh
+++ modules/rubyapp/files/start-rubyapp.sh
@@ -4,4 +4,4 @@
 # $2 => application directory (appname.domain)
 # $3 => application port
 
-su - deploy -c "( cd ${1}.${2} ; bundle exec passenger start -e production -a 127.0.0.1 -p ${3} -d )"
+su - deploy -c "( cd ${1}.${2} ; bundle install ; bundle exec passenger start -e production -a 127.0.0.1 -p ${3} -d )"
