e2083576fb17ba7fb1ffd8ea449b6eedff642d1f
cnacorrea <carlos.nilton@gmail.com>
2013-09-27 16:09:39 -0300

    proper commenting for site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,33 +1,36 @@
+# classes should always include these modules
 node default {
 	include puppet
 	include sudo
 	include ntp
 }
 
+# to do some testing over my ubuntu vms
 node ubuntuvm inherits default {
 }
 
+# a default node for unimed' systems
 node unimed inherits default {
 	include user::virtual
 	include user::redes-unimed
 }
 
-node unimed-banco inherits unimed {
+# database systems must have dba account
+node unimed-database inherits unimed {
 	include user::dba-unimed
 }
 
+# for testing over my "desktop" MBP
 node 'billmbp' {
 	include puppet
 }
 
-node 'otrs' inherits default {
-	include repos::rpmforge
+# 972 unicoo
+node rjolnx008 inherits unimed-database {
 }
 
-node rjolnx008 inherits unimed-banco {
-}
-
-node rjofedlx212 inherits unimed-banco {
+# 972 app.d
+node rjofedlx212 inherits unimed-database {
 	include jdk160_18
 
 	class { "jboss711":
@@ -37,7 +40,8 @@ node rjofedlx212 inherits unimed-banco {
 	include union
 }
 
-node rjofedlx213 inherits unimed-banco {
+# 972 app.h
+node rjofedlx213 inherits unimed-database {
 	rubyapp::deploy { "trabalheconosco":
 		app_name   => "trabalheconosco",
 		app_domain => "h.unimedrj.com.br",
@@ -55,16 +59,20 @@ node rjofedlx213 inherits unimed-banco {
 	}
 }
 
-node rjofedlx214 inherits unimed-banco {
+# 972 + ProviderIT Continuous Integration
+node rjofedlx214 inherits unimed-database {
 }
 
-node rjofedlx215 inherits unimed-banco {
+# 972 DwWeb production
+node rjofedlx215 inherits unimed-database {
 }
 
-node rjofedlx219 inherits unimed-banco {
+# 972 app.s
+node rjofedlx219 inherits unimed-database {
 }
 
-node rjodmzlx003 inherits unimed-banco {
+# 972 web production
+node rjodmzlx003 inherits unimed-database {
 	rubyapp::deploy { "trabalheconosco":
 		app_name   => "trabalheconosco",
 		app_domain => "unimedrj.com.br",
@@ -75,57 +83,57 @@ node rjodmzlx003 inherits unimed-banco {
 }
 
 # 341 200
-node 'unicoo.angra.unimed.com.br' inherits unimed-banco {
+node 'unicoo.angra.unimed.com.br' inherits unimed-database {
 }
 
 # 248 200
-node 'bpilnx010' inherits unimed-banco {
+node 'bpilnx010' inherits unimed-database {
 }
 
 # 248 201
-node 'homologa.barrapirai.unimed.com.br' inherits unimed-banco {
+node 'homologa.barrapirai.unimed.com.br' inherits unimed-database {
 }
 
 # 052 200
-node 'bmalnx004.barramansa.unimed.com.br' inherits unimed-banco {
+node 'bmalnx004.barramansa.unimed.com.br' inherits unimed-database {
 }
 
 # 052 201
-node 'bmalnx08.barramansa.unimed.com.br' inherits unimed-banco {
+node 'bmalnx08.barramansa.unimed.com.br' inherits unimed-database {
 }
 
 # 231 200
-node 'unicoo.costaverde.unimed.com.br' inherits unimed-banco {
+node 'unicoo.costaverde.unimed.com.br' inherits unimed-database {
 }
 
 # 231 201
-node 'homologa.costaverde.unimed.com.br' inherits unimed-banco {
+node 'homologa.costaverde.unimed.com.br' inherits unimed-database {
 }
 
 # 023 200
-node 'oralnx001.novaiguacu.unimed.com.br' inherits unimed-banco {
+node 'oralnx001.novaiguacu.unimed.com.br' inherits unimed-database {
 }
 
 # 077 200
-node 'unicoo.itaperuna.unimed.com.br' inherits unimed-banco {
+node 'unicoo.itaperuna.unimed.com.br' inherits unimed-database {
 }
 
 # 296 200
-node 'producao.mvalenca.unimed.com.br' inherits unimed-banco {
+node 'producao.mvalenca.unimed.com.br' inherits unimed-database {
 }
 
 # 296 201
-node 'homologa.mvalenca.unimed.com.br' inherits unimed-banco {
+node 'homologa.mvalenca.unimed.com.br' inherits unimed-database {
 }
 
 # 126 200
-node 'unicoo.noroesterj.unimed.com.br' inherits unimed-banco {
+node 'unicoo.noroesterj.unimed.com.br' inherits unimed-database {
 }
 
 # 126 201
-node 'homologa.noroesterj.unimed.coop.br' inherits unimed-banco {
+node 'homologa.noroesterj.unimed.coop.br' inherits unimed-database {
 }
 
 # 205 200
-node 'oracle.campos.unimed.com.br' inherits unimed-banco {
+node 'oracle.campos.unimed.com.br' inherits unimed-database {
 }
